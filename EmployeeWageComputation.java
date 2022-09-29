f52274dd85470ae42c0a0a7c8d48d1173d05d2b7
Vikas1028 <vikasbhandekar2018@gmail.com>
2022-09-29 13:30:30 +0530

    Add: Part time Employee & Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,8 +4,9 @@ import java.util.Random;
 
 public class EmployeeWageComputation {
 
-	public int Presenti;
+	public int Presenti,Presenti2;
 	public int WagePerHour, FullDayHours, HalfDayHours, Hours;
+	public int PartTimeWagePerHour;
 
 	// Start with displaying welcome message
 	public void printMessage() {
@@ -42,16 +43,43 @@ public class EmployeeWageComputation {
 
 	}
 
+	//Add part time Employee and Wage
+	public void AddPartTimeEmployee() {
+		System.out.println("Part Time Employee Added");
+		PartTimeWagePerHour=10;
+		FullDayHours=8;
+		HalfDayHours=4;
+		Random num2 = new Random();
+		Presenti2 = num2.nextInt(2);
+		if (Presenti2 == 0) {
+			System.out.println("Part Time Employee is Absent");
+		} else {
+			System.out.println("Part Time Employee is Present");
+		}
+		if (Presenti2 == 1) {
+			Random num3 = new Random();
+			Hours = num3.nextInt(2);
+			if (Hours == 1) {
+				System.out.println("Today Part Time Employee work Full day");
+				System.out.println("Today Part Time Employee Wage is " + PartTimeWagePerHour * FullDayHours+" Rs.");
+			} else {
+				System.out.println("Today Part Time Employee work Half day");
+				System.out.println("Today Part Time Employee Wage is " + PartTimeWagePerHour * HalfDayHours+" Rs.");
+			}
+		}
+		
+	}
+
 	public static void main(String[] args) {
 
-		UC2_EmployeeDailyWage call = new UC2_EmployeeDailyWage();
+		UC3_AddPartTimeEmployee call = new UC3_AddPartTimeEmployee();
 
 		call.printMessage();
 
 		call.checkEmployeeAttendance();
 
 		call.calculateEmployeeDailyWage();
+		
+		call.AddPartTimeEmployee();
 	}
 }
-
-
